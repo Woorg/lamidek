262e5b8f4fed2867b70eaacec2e8e253bfe35bb7
Igor Gorlov <gorlov35@gmail.com>
2021-03-27 17:07:28 +0300

    fix: hide maps on mobile at contact page

--- markup/blocks/contact/contact.js
+++ markup/blocks/contact/contact.js
@@ -9,7 +9,20 @@ const contactMapShow = () => {
         $(this).parent().next().toggleClass('contact__col_map_hidden');
     });
 
+    let $mapContainer = $('.contact__col');
+
+    // contact__col_map_hidden
+
+    // console.log($(document).width());
+
+    if ($(document).width() < 800 ) {
+        $mapContainer.addClass('contact__col_map_hidden');
+    }
+
 }
+
+
+
 
 
 export default contactMapShow;
